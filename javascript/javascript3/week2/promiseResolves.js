18059aebc222c443a694535b4f80f861fc5a621a
Kammal <kamalrsa@gmai.com>
2020-09-25 13:05:03 +0200

    promise resolved

--- javascript/javascript3/week2/promiseResolves.js
+++ javascript/javascript3/week2/promiseResolves.js
@@ -29,3 +29,47 @@ setPromise.then(() => {
   console.log("called after 9 sec");
 });
 // get current location promise
+
+function currentLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      resolve(
+        `Latitude: ${position.coords.latitude} Longitude: ${position.coords.longitude}`
+      );
+    }, reject);
+  })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+currentLocation();
+
+// fetching and waiting
+
+function fetchAPI() {
+  fetch("http://api.open-notify.org/astros.json")
+    .then((response) => response.json())
+    .then((data) => console.log("astros", data));
+}
+function accessData() {
+  setTimeout(() => {
+    fetchAPI();
+  }, 4000);
+}
+accessData();
+// fetching and waiting async/await away
+async function fetchAstors() {
+  const response = await fetch("http://api.open-notify.org/astros.json");
+  const astrosData = await response.json();
+  console.log("astros", astrosData);
+  //return astrosData;
+}
+function astros() {
+  setTimeout(() => {
+    fetchAstors();
+  }, 4000);
+}
+astros();
