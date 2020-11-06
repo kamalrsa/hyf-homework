aba07bac547af292dc15c1a78e1570acb6b3966a
Kammal <kamalrsa@gmai.com>
2020-11-06 13:29:06 +0100

    meal-sharing

--- nodejs/week2/meal-sharing/src/backend/api/meals-router.js
+++ nodejs/week2/meal-sharing/src/backend/api/meals-router.js
@@ -10,23 +10,40 @@ router.get("/", async (request, response) => {
       item.title.toLowerCase().includes(request.query.title.toLowerCase())
     );
     response.send(titleResult);
-  }
-  if (request.query.maxPrice) {
-    const price = meals.filter(
-      (item) => item.price < parseInt(request.query.maxPrice)
-    );
-    response.send(price);
+  } else if (request.query.maxPrice) {
+    if (!parseInt(request.query.maxPrice)) {
+      return response
+        .status(400)
+        .send(`There is no meal with ${request.params.maxPrice} `);
+    } else {
+      const price = meals.filter(
+        (item) => item.price < parseInt(request.query.maxPrice)
+      );
+      response.send(price);
+    }
   } else if (request.query.createdAfter) {
-    const createdAfter = new Date(request.query.createdAfter);
-    const created = meals.filter(
-      (item) => new Date(item.createdAt) > createdAfter
-    );
-    response.send(created);
+    if (!parseInt(request.query.createdAfter)) {
+      return response
+        .status(400)
+        .send(`There is no meal with ${request.params.maxPrice} `);
+    } else {
+      const createdAfter = new Date(request.query.createdAfter);
+      const created = meals.filter(
+        (item) => new Date(item.createdAt) > createdAfter
+      );
+      response.send(created);
+    }
   } else if (request.query.limit) {
-    const limitMeal = meals.filter(
-      (item) => meals.indexOf(item) < parseInt(request.query.limit)
-    );
-    response.send(limitMeal);
+    if (!parseInt(request.query.limit)) {
+      return response
+        .status(400)
+        .send(`There is no meal with ${request.params.maxPrice} `);
+    } else {
+      const limitMeal = meals.filter(
+        (item) => meals.indexOf(item) < parseInt(request.query.limit)
+      );
+      response.send(limitMeal);
+    }
   } else {
     response.send(meals);
   }
