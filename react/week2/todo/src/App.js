79111ae8a316df20da37201ec36da0337d2c1237
Kammal <kamalrsa@gmai.com>
2020-11-26 14:38:08 +0100

    todo-list

--- react/week2/todo/src/App.js
+++ react/week2/todo/src/App.js
@@ -1,12 +1,14 @@
 import Counter from "./Counter";
 import { Fibonacci } from "./Fibonacci";
+import ToDoList from "./ToDoList";
 export function App() {
   return (
     <div>
       <h3>Fibonacci Series</h3>
       <Fibonacci></Fibonacci>
-      <h1>TODoList</h1>
+
       <Counter></Counter>
+      <ToDoList></ToDoList>
     </div>
   );
 }
