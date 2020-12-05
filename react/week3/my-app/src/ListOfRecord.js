09c8f8586831383afc4ab910460879838422aa78
Kammal <kamalrsa@gmai.com>
2020-12-05 10:15:17 +0100

    employee-shift

---
+++ react/week3/my-app/src/ListOfRecord.js
@@ -0,0 +1,14 @@
+import React from "react";
+const ListOfRecord = (props) => {
+  return (
+    <>
+      <div>
+        Name:{props.text.eName}
+        <br></br>Start Time:{props.text.sTime}
+        <br></br> End Time:{props.text.eTime}
+      </div>
+      <div></div>
+    </>
+  );
+};
+export default ListOfRecord;
